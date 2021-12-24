2ef41af066081cde0f9bf8ee3e04b2fe798191d4
CaKa12 <83893877+MortyHub@users.noreply.github.com>
2021-12-24 02:45:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+words = ['Ear', 'Muffin', 'Clock','Crave',"Crab","Consistent","Cake","Robber","Clippo","Hippo","Girl","Man","Boy","Female","Intestine","Stomach","Acid","Crappy","Fundementals","Computer","Science","Robotics","Seperate","Defeat","Insist","Letter","Random","Variable","Generator","OwO","Rob","Is","Cringe","Ha","Lol","Crazy","Five","Four","Three","Two","One", "Zero", "Rocket","Idea","Five","_", "Above","Illusion","Federal","Politician","Politics","Funny","Six","Seven","Eight","Nine","Ten", "Available", "Behead","Offend","Dive", "Sense", "Dynamic", "Studio", "Lights", "Proportion", "Fame", "CourtShip", "Court","Law","Offender","Officer","Medic","Sargent","Paramedic","Parry", "Plat","Python","CPP", "C", "Java","JavaScript", "Script", "Code", "Ruby", "Divi", "If", "Statement", "OverCame", "Hamilton", "The", "World", "Harbor", "Ship", "Spot", "Him", "Her", "America", "Russia", "China", "Afirca", "Britian", "Great", "Crutch", "Digress", "Matter", "Crevice", "Power", "Vice", "President","_","_","_","_","_", "_", "_", "_", "_", "_"]
+
+Var = ''
+for i in range(1,4):
+	Var += words[random.randrange(120)]
+print(Var)
+
